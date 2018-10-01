3cbedff0cffdd8c62ce663099bdae9ae58fe8dd6
Hannah Norish <Hannahjn@outlook.com>
2018-10-01 17:46:05 -0400

    cesium file added

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 // any time we use an INERTIAL frame in Cesium, it needs to know where to find it's
 // ASSET folder on the web.  The SunMoonLights computation uses INERTIAL frames, so
 // so we need to put the assets on the web and point Cesium at them
-var CESIUM_BASE_URL = '../resources/cesium/';
+var CESIUM_BASE_URL = './cesium/';
 // grab some handles on APIs we use
 var Cesium = Argon.Cesium;
 var Cartesian3 = Argon.Cesium.Cartesian3;
